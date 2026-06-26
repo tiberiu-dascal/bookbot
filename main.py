d5d1fe3a458dae2c622449a779ebb7315609b42f
Tiberiu Claudiu <tdascal@opentext.com>
2026-06-26 16:44:24 +0300

    Re-start

--- main.py
+++ main.py
@@ -1,53 +1,21 @@
-import sys
-from stats import count_words
+from stats import get_num_words, characters_count
 
 
-def count_letters(text):
-    letters_count = {}
-    for letter in text:
-        letter = letter.lower()
-        if letter.isalpha():
-            if letter in letters_count:
-                letters_count[letter] += 1
-            else:
-                letters_count[letter] = 1
-    return letters_count
+def get_book_text(file_path):
 
+    book_content: str = ""
+    with open(file_path) as f:
+        book_content = f.read()
 
-def sort_on(d):
-    return d["num"]
-
-
-def chars_dict_to_sorted_list(chars_dict):
-    sorted_list = []
-    for ch in chars_dict:
-        sorted_list.append({"char": ch, "num": chars_dict[ch]})
-    sorted_list.sort(reverse=True, key=sort_on)
-    return sorted_list
+    return book_content
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python3 main.py <path_to_book>")
-        sys.exit(1)
-    else:
-        book = sys.argv[1]
-
-    with open(book) as f:
-        file_contents = f.read()
-    print(f"--- Begin report of {book} ---")
-    print(f"{count_words(file_contents)} words found in the document\n")
-
-    counted_letters = count_letters(file_contents)
-    chars_sorted_list = chars_dict_to_sorted_list(counted_letters)
-
-    for item in chars_sorted_list:
-        if not item["char"].isalpha():
-            continue
-        print(f"- '{item['char']}: {item['num']}'")
-
-    print("--- End report ---")
+    frankenstein_text = get_book_text("books/frankenstein.txt")
+    w_count = get_num_words(frankenstein_text)
+    chars_counter = characters_count(frankenstein_text)
+    print(f"Found {w_count} total words.")
+    print(chars_counter)
 
 
-if __name__ == "__main__":
-    main()
+main()
--- stats.py
+++ stats.py
@@ -1,3 +1,20 @@
+def get_num_words(text) -> int:
+    words = text.split()
+    return len(words)
+
+
+def characters_count(text: str) -> dict[str:int]:
+    characters_counter = {}
+    characters = list(text)
+    for char in characters:
+        char = char.lower()
+        if char not in characters_counter:
+            characters_counter[char] = 1
+        else:
+            characters_counter[char] += 1
+        # print(characters_counter)
+    return characters_counter
+
 
 def count_words(text):
     words = text.split()
